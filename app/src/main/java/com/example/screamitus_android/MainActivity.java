ae83e2b852444c6dada3c259b13c1a9b35a62c96
prabh11bhullar <kaur6363satwinder@gmail.com>
2019-09-19 10:50:12 -0400

    first commit

--- app/src/main/java/com/example/screamitus_android/MainActivity.java
+++ app/src/main/java/com/example/screamitus_android/MainActivity.java
@@ -9,7 +9,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
