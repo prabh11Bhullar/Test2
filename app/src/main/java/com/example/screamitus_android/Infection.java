d5519e3d6f64d7fb7e4ecd199ad2806a7f8b093a
prabh11bhullar <kaur6363satwinder@gmail.com>
2019-09-19 12:10:38 -0400

    Test Failed

--- app/src/main/java/com/example/screamitus_android/Infection.java
+++ app/src/main/java/com/example/screamitus_android/Infection.java
@@ -1,12 +1,27 @@
 package com.example.screamitus_android;
 
 public class Infection {
+    int totalinsructors;
+    //n is no. of days
+    int instructor=1;
     public double calculateTotalInfected(int n) {
         if (n <= 0) {
            return  -1;
         }
+        else
+        {
+           if(n==1 &&n<=5)
+           {
+               totalinsructors=n*5;
+           }
+           else if(n>7)
+           {
+               totalinsructors=n*8;
+           }
 
-        return 0;
+        }
+
+        return totalinsructors;
     }
 
 }
